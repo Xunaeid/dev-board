77dddf7d6194207fac62d44fb1e6fa251c8818ae
Xuna <mail.junaeid@gmail.com>
2025-03-02 23:42:58 +0600

    theme issues fixed

--- script/theme.js
+++ script/theme.js
@@ -1,22 +1,24 @@
-// const theme = document.getElementById("theme")
-// const body = document.getElementById("body")
-// // body.style.backgroundColor()
+const theme = document.getElementById("theme")
+const body = document.getElementById("body")
+// body.style.backgroundColor()
 
-//  theme.addEventListener("click", function(){
-
-//  })
+ theme.addEventListener("click", function(){
+    let color = ["#B4EBE6","blue", "red", "#80CBC4" , "#C599B6" ,"#2DAA9E"]
+    let random =Math.floor(Math.random()*color.length)
+    body.style.background = color[random]
+})
 
 
  
-// let color = ["red","blue","black"]
+// let color = [body.style.background = "teal",body.style.background = "blue",body.style.background = "red"]
 
-// let random =Math.floor(Math.random()*color.length)
-// console.log(color[random])
+let random =Math.floor(Math.random()*color.length)
+console.log(color[random])
 
 
-// //  bg-teal-100
-// //  bg-red-100
-// //  bg-gray-100
-// //  bg-orange-100
-// //  bg-blue-100
-// //  bg-green-100
+//  bg-teal-100
+//  bg-red-100
+//  bg-gray-100
+//  bg-orange-100
+//  bg-blue-100
+//  bg-green-100
